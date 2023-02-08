8cc06e15f517f4312607db097eb884a2c6f296cb
geraldng1407 <g-e-r-a-l-d@hotmail.com>
2023-02-08 23:30:02 +0800

    Standardize Header for FacilityInfo Page

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -9,91 +9,96 @@ import FilterPage from "../pages/Filter";
 import HomePage from "../pages/Home";
 import ListingsPage from "../pages/Listings";
 import MapPage from "../pages/Map";
+import ScanningPage from "../pages/Scanning";
 
 const Stack = createNativeStackNavigator();
 const HomeStack = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Group
-        screenOptions={{
-          // headerTitleStyle: {
-          //   fontWeight: "bold",
-          //   color: "white",
-          //   fontSize: 25,
-          // },
-          // headerBackground: () => (
-          //   <LinearGradient
-          //     colors={["#879fe9", "#2349c2"]}
-          //     style={[StyleSheet.absoluteFill]}
-          //   />
-          // ),
-          // headerStyle: {
-          //   height: 150,
-          // },
-          headerStyle: {
-            backgroundColor: "#2349cf",
-            height: 50,
-          },
-          headerShadowVisible: false,
-          headerTitleAlign: "center",
-          headerTitle: "Home",
-          headerTitleStyle: {
-            color: "white",
-          },
-        }}
-      >
-        <Stack.Screen
-          options={{ headerTitle: "Home" }}
-          name="Default"
-          component={HomePage}
-        />
-        <Stack.Screen
-          options={{ headerTitle: "Bookings" }}
-          name="Bookings"
-          component={BookingPage}
-        />
-        <Stack.Screen
-          options={{ headerTitle: "Listings" }}
-          name="Listings"
-          component={ListingsPage}
-        />
-        <Stack.Screen
-          options={{ headerTitle: "Home" }}
-          name="LandingPage"
-          component={ActualHome}
-        />
-        <Stack.Screen
-          options={{ headerTitle: "Search" }}
-          name="Filter"
-          component={FilterPage}
-        />
-        <Stack.Screen
-          name="Map"
-          options={{ headerTitle: "Section View" }}
-          component={MapPage}
-        />
-      </Stack.Group>
-
-      <Stack.Screen
-        name="FacilityInfo"
-        component={FacilityInformation}
-        options={{ headerShown: false }}
-      />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Group
+                screenOptions={{
+                    headerTitleStyle: {
+                        fontWeight: "bold",
+                        color: "white",
+                        fontSize: 25,
+                    },
+                    headerBackground: () => (
+                        <LinearGradient
+                            colors={["#879fe9", "#2349c2"]}
+                            style={[StyleSheet.absoluteFill]}
+                        />
+                    ),
+                    headerStyle: {
+                        height: 150,
+                    },
+                    headerStyle: {
+                        backgroundColor: "#2349cf",
+                        height: 50,
+                    },
+                    headerShadowVisible: false,
+                    headerTitleAlign: "center",
+                    headerTitle: "Home",
+                    headerTitleStyle: {
+                        color: "white",
+                    },
+                }}
+            >
+                <Stack.Screen
+                    options={{ headerTitle: "Home" }}
+                    name="Default"
+                    component={HomePage}
+                />
+                <Stack.Screen
+                    options={{ headerTitle: "Bookings" }}
+                    name="Bookings"
+                    component={BookingPage}
+                />
+                <Stack.Screen
+                    options={{ headerTitle: "Listings" }}
+                    name="Listings"
+                    component={ListingsPage}
+                />
+                <Stack.Screen
+                    options={{ headerTitle: "Home" }}
+                    name="LandingPage"
+                    component={ActualHome}
+                />
+                <Stack.Screen
+                    options={{ headerTitle: "Search" }}
+                    name="Filter"
+                    component={FilterPage}
+                />
+                <Stack.Screen
+                    name="Map"
+                    options={{ headerTitle: "Section View" }}
+                    component={MapPage}
+                />
+                <Stack.Screen
+                    name="FacilityInfo"
+                    component={FacilityInformation}
+                    options={{ headerTitle: "Facility Information"}}
+                />
+                <Stack.Screen
+                    name="Scanning"
+                    component={ScanningPage}
+                    options={{ headerTitle: "Scan QR code"}}
+                />
+            </Stack.Group>
+        </Stack.Navigator>
+    );
 };
 
 export default HomeStack;
 
 const styles = StyleSheet.create({
-  main: {
-    backgroundColor: "#F4F6FB",
-  },
-  linearGradient: {
-    height: 150,
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
-    shadowColor: "#000",
-    elevation: 25,
-  },
+    main: {
+        backgroundColor: "#F4F6FB",
+    },
+    linearGradient: {
+        height: 150,
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        shadowColor: "#000",
+        elevation: 25,
+    },
 });
