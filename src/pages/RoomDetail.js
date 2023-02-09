79f7faae28ef36fed50f08f2c6d6252ac1161500
ynseah.2021 <ynseah.2021@smu.edu.sg>
2023-02-09 22:44:40 +0800

    Partial completion of roomdetail page and adding button on home page

--- src/pages/RoomDetail.js
+++ src/pages/RoomDetail.js
@@ -5,7 +5,25 @@ const RoomDetailsPage = ({ navigation }) => {
   return (
     <View>
         <LinearGradient style={{ height: "100%" }} colors={["#2349cf", "#B79fe9"]}>
-            <Text>This is Booking</Text>
+            <View>
+              <Text style={styles.titleText}>
+                Group Study Room (GSR) 3-2
+              </Text>
+            </View>
+
+            <View  style={{marginTop: 10}}>
+                <Text style={styles.contentText}>
+                  BUILDING:{"\n"}
+                  School of Computing and Information Systems 1{"\n"}  {"\n"}
+                  CAPACITY: {"\n"} 
+                  6 {"\n"} {"\n"}
+                  CO-BOOKER: {"\n"}
+                  1 {"\n"}{"\n"}
+                  STATUS: {"\n"}
+                  Available for booking {"\n"}
+              </Text>
+                
+            </View>
         </LinearGradient>
       
     </View>
@@ -14,3 +32,22 @@ const RoomDetailsPage = ({ navigation }) => {
 
 
 export default RoomDetailsPage;
+
+const styles = StyleSheet.create({
+    titleText: {
+      fontWeight: "medium",
+      fontSize: 20,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      color: "white"
+    },
+
+    contentText: {
+      fontWeight: "medium",
+      fontSize: 15,
+      paddingHorizontal: 10,
+      paddingVertical: 5,
+      color: "white",
+      // lineHeight: 25
+    },
+})
