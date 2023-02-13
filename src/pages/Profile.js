1fa66078f860b99df9a8d2a7d615a89c3f81804a
Yogesh352 <80184454+Yogesh352@users.noreply.github.com>
2023-02-13 15:04:35 +0800

    standardise styling

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,8 +2,15 @@ import { Button, Text, View, StyleSheet } from "react-native";
 
 const ProfilePage = ({ navigation }) => {
   return (
-    <View style={{ padding: 10, flex: 1, justifyContent: "space-evenly" }}>
-      <Text>This is profile</Text>
+    <View
+      style={{
+        backgroundColor: "#1a2222",
+        padding: 10,
+        flex: 1,
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Text style={{ color: "white" }}>This is profile</Text>
     </View>
   );
 };
