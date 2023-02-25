87e1153ae928a4c3b3420d1bf8e2a70c4f182f9e
liqipotato <89569278+liqipotato@users.noreply.github.com>
2023-02-25 14:50:16 +0800

    improved timepicker and reset button

--- src/pages/Filter.js
+++ src/pages/Filter.js
@@ -8,15 +8,54 @@ import TimeSelection from "../components/Filter/TimeSelection";
 
 const FilterPage = ({ navigation }) => {
   const reset = () => {
-    setStartTimeChange("");
-    setEndTimeChange("");
+    setStartTimeChange(defaultTime);
+    setEndTimeChange(defaultTime);
   };
+
+  const formatTimeByOffset = (dateString, offset) => {
+    if (!dateString) return ''
+    if (dateString.length === 0) return ''
+  
+    const year = dateString.slice(0, 4)
+    const month = dateString.slice(5, 7)
+    const day = dateString.slice(8, 10)
+    const hour = dateString.slice(11, 13)
+    const minute = dateString.slice(14, 16)
+    const second = dateString.slice(17, 19)
+    const dateObject = new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`)
+  
+    const currentHours = dateObject.getHours()
+    dateObject.setHours(currentHours + offset)
+    const newDateString = dateObject
+      .toISOString()
+      .replace('T', ' ')
+      .slice(0, 16)
+    
+    return `${newDateString}`
+  }
+
+  let nowString = JSON.stringify(new Date());
+  let nowArray = nowString.split("T");
+  let now = nowArray[0].substring(1, 11) + " " + nowArray[1].substring(0, 8);
+  let defaultTime = formatTimeByOffset(now, 8).toLocaleString();
+  let defaultTimePicker = Date.parse(formatTimeByOffset(now, 8).toLocaleString());
+  console.log(defaultTimePicker);
+  console.log(defaultTime);
+  const [startTimeSelected, setStartTimeChange] = React.useState(defaultTime);
+  const [endTimeSelected, setEndTimeChange] = React.useState(defaultTime);
 
   return (
     <View style={styles.content}>
       <View style={styles.container}>
         <Calendar />
-        <TimeSelection />
+        <TimeSelection 
+          defaultTime={defaultTime} 
+          startTimeSelected={startTimeSelected} 
+          setStartTimeChange={setStartTimeChange}
+          endTimeSelected={endTimeSelected}
+          setEndTimeChange={setEndTimeChange}
+          defaultTimePicker={defaultTimePicker}
+        />
         <View style={styles.facilityTypeDropdown}>
           <FacilityTypeDropdown />
         </View>
